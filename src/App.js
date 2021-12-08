edc10e39d392150b9e4977aacf1f8bd4f4590f91
dcampbellcreative <dougcampbell1990@gmail.com>
2021-12-08 14:57:54 -0500

    added integration testing into app.tst.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { render } from 'enzyme';
+import { getEvents } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <NumberOfEvents />
-      <EventList />
-    </div>
-  );
+class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents />
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/CitySearch.js
+++ src/CitySearch.js
@@ -21,7 +21,9 @@ class CitySearch extends Component {
 	handleItemClicked = (suggestion) => {
 		this.setState({
 			query: suggestion
-		})
+		});
+
+		this.props.updateEvents(suggestion);
 	}
 
 	render() {
