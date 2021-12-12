9624965907d90b59f5db7dcd94bc38e835f7a324
dcampbellcreative <dougcampbell1990@gmail.com>
2021-12-12 12:05:16 -0500

    created integration test for numberOfEvents props, pass numberOfEvents as prop to component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -40,11 +40,20 @@ describe('<App /> integration', () => {
 		AppWrapper.unmount();
 	});
 
-	test('App pasess "locations" state as a prop to CitySearch', () => {
+	test('App passes "locations" state as a prop to CitySearch', () => {
 		const AppWrapper = mount(<App />);
 		const AppLocationsState = AppWrapper.state('locations');
 		expect(AppLocationsState).not.toEqual(undefined);
 		expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
+		AppWrapper.unmount();
+	});
+
+	//FAILS
+	test('App passes "numberOfEvents" state as a prop to NumberOfEvents', () => {
+		const AppWrapper = mount(<App />);
+		const AppNumberOfEventsState = AppWrapper.state('numberOfEvents');
+		expect(AppNumberOfEventsState).not.toEqual(undefined);
+		expect(AppWrapper.find(NumberOfEvents).props().events).toEqual(AppNumberOfEventsState);
 		AppWrapper.unmount();
 	});
 
@@ -63,7 +72,7 @@ describe('<App /> integration', () => {
 		AppWrapper.unmount();
 	})
 
-	test('get list of all evnets when user selects "See all cities"', async () => {
+	test('get list of all events when user selects "See all cities"', async () => {
 		const AppWrapper = mount(<App />);
 		const suggestionItems = AppWrapper.find(CitySearch).find('.suggestions li');
 		await suggestionItems.at(suggestionItems.length - 1).simulate('click');
