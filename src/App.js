80eefbe24556106ea90e5848f777f1da0843b02a
dcampbellcreative <dougcampbell1990@gmail.com>
2021-12-14 16:59:53 -0500

    update css styling

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 className='title'>Meet</h1>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
