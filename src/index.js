9bbc93e95af809cae96c26b88d3bc55fa5e46ea1
dcampbellcreative <dougcampbell1990@gmail.com>
2021-12-16 17:16:55 -0500

    add atatus for testing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('5c00b30e6c0e410fb95b683a011d6278').install();
 
 ReactDOM.render(
   <React.StrictMode>
